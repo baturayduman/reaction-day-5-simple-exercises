1de308d90ef0a1d4e2632c096162effe18980a7e
Baturay Duman <67900152+baturayduman@users.noreply.github.com>
2024-12-09 20:02:44 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";  // Sadece useState import ediyorsunuz
+import "./App.css";  // CSS dosyasını import ediyorsunuz
+
+
+export default function App() {
+
+  const [farePozisyonu, setFarePozisyonu] = useState({ x: 0, y: 0 });
+
+
+  return (
+    <div className="App" >
+      <h1> FARENİN BULUNDUĞU KONUM : </h1>
+      <h2>
+
+        X'İN BULUNDUĞU KONUM = {farePozisyonu.x}
+
+        Y'NİN BULUNDUĞU KONUM = {farePozisyonu.y}
+
+
+      </h2>
+
+      <div
+        style={{ width: "100vw", height: "100vh" }}
+        onMouseMove={(e) => {
+          setFarePozisyonu({ x: e.clientX, y: e.clientY });
+
+        }}>
+
+      </div>
+    </div>
+  )
+}
